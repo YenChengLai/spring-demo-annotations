6fb954b4ceb3ab7d6a549900fbd20640ec320bf1
Frank Lai <frank810618@gmail.com>
2021-03-02 16:11:43 +0800

    add dependency injection with annotation

---
+++ src/com/java/springdemo/TennisCoach.java
@@ -0,0 +1,13 @@
+package com.java.springdemo;
+
+import org.springframework.stereotype.Component;
+
+@Component("tennisCoach")
+public class TennisCoach implements Coach {
+
+	@Override
+	public String getDailyWorkout() {
+		return "practice back hand volley";
+	}
+
+}
